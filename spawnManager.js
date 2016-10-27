a84b992a4254e187249bd95f87e981620e121d60
Iain Earl <synapse791@gmail.com>
2016-10-27 11:54:05 +0100

    Altering console messages

--- spawnManager.js
+++ spawnManager.js
@@ -18,13 +18,13 @@ class SpawnManager {
     if (this.spawns.length > 0) {
       let counts = this.roleManager.getCounts();
       if (counts.miners < this.requirements.miners) {
-        console.log('creating new miner')
+        console.log(`creating miner ${counts.miners + 1} of ${this.requirements.miners}`)
         this.roleManager.roles.miner.create(this.spawns[0]);
       } else if (counts.workers < this.requirements.workers) {
-        console.log('creating new worker')
+        console.log(`creating worker ${counts.workers + 1} of ${this.requirements.workers}`)
         this.roleManager.roles.worker.create(this.spawns[0]);
       } else if (counts.builders < this.requirements.builders) {
-        console.log('creating new builder')
+        console.log(`creating builder ${counts.builders + 1} of ${this.requirements.builders}`)
         this.roleManager.roles.builder.create(this.spawns[0]);
       }
     }
